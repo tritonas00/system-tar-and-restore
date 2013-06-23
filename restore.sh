5c9f6e96fa428d9174a5037248e3ca8f209747f8
tritonas00 <tritonas00@gmail.com>
2013-06-24 02:57:25 +0300

    fixed protected url name

--- restore.sh
+++ restore.sh
@@ -2060,7 +2060,7 @@ elif [ $BRinterface = "Dialog" ]; then
     fi
 
     while [ ! -f /mnt/target/fullbackup ]; do
-      REPLY=$(dialog  --cancel-label Quit --menu "Select backup file. Choose an option:" 13 50 13  File "local file" URL "remote file" Protected "protected server" 2>&1 1>&3)
+      REPLY=$(dialog  --cancel-label Quit --menu "Select backup file. Choose an option:" 13 50 13  File "local file" URL "remote file" "Protected URL" "protected remote file" 2>&1 1>&3)
       if [ $? = "1" ]; then
         if [  "x$BRfsystem" = "xbtrfs" ] && [ "x$BRrootsubvol" = "xy" ]; then
           unmount_only_in_subvol
@@ -2101,11 +2101,10 @@ elif [ $BRinterface = "Dialog" ]; then
             BRpath="${BRpath//\\/ }"
           fi
         done
-
-      elif [ $REPLY = "URL" ] || [ $REPLY = "Protected" ]; then
+      elif [ $REPLY = "URL" ] || [ $REPLY = "Protected URL" ]; then
         unset BRfile
         BRurl=$(dialog  --no-cancel --inputbox "Enter the URL for the backup file:" 8 50 2>&1 1>&3)
-        if [  $REPLY = "Protected" ]; then
+        if [  $REPLY = "Protected URL" ]; then
           BRusername=$(dialog --no-cancel --inputbox "Username:" 8 50 2>&1 1>&3)
           BRpassword=$(dialog --no-cancel --insecure --passwordbox "Password:" 8 50 2>&1 1>&3)
           ( wget --user=$BRusername --password=$BRpassword  -O /mnt/target/fullbackup $BRurl --tries=2 || touch /tmp/wget_error ) 2>&1 |
@@ -2123,7 +2122,6 @@ elif [ $BRinterface = "Dialog" ]; then
               rm /mnt/target/fullbackup 2>/dev/null
             fi
           fi
-
         elif [ $REPLY = "URL" ]; then
           ( wget -O /mnt/target/fullbackup $BRurl --tries=2 || touch /tmp/wget_error ) 2>&1 |
           sed -nru '/[0-9]%/ s/.* ([0-9]+)%.*/\1/p' | dialog  --gauge "Downloading..." 0 50

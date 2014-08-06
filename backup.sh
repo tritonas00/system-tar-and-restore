4fc0a5fd80ee6731dbcba7fb47b1abe1da702ee4
tritonas00 <tritonas00@gmail.com>
2014-08-07 02:51:23 +0300

    updated version

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BR_VERSION="System Tar & Restore 4"
+BR_VERSION="System Tar & Restore 4.0"
 BR_SEP="::"
 
 if [ -f /etc/backup.conf ]; then
--- restore.sh
+++ restore.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BR_VERSION="System Tar & Restore 4"
+BR_VERSION="System Tar & Restore 4.0"
 
 BR_EFI_DETECT_DIR="/sys/firmware/efi"
 BR_SEP="::"

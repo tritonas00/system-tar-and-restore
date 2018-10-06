3fbfb0d17fdf418ffafc8f2c90826e8f189bca9d
tritonas00 <tritonas00@gmail.com>
2018-10-06 18:19:09 +0300

    dont freeze on cancel, keep the user informed

--- star-gui.sh
+++ star-gui.sh
@@ -283,38 +283,33 @@ set_args() {
   fi
 }
 
-kill_umount() {
-  kill -9 -$(tail -1 /tmp/wr_pid)
-  echo "Process ID $(tail -1 /tmp/wr_pid) killed" > /tmp/wr_log
+clean_umount() {
+  cd /
+  sleep 1
+  if [ -n "$RT_ROOT" ]; then parts_array+=("/"="${RT_ROOT%% *}"); fi
+  if [ -n "$RT_ESP" ]; then parts_array+=("$RT_ESP_MOUNTPOINT"="${RT_ESP%% *}"); fi
+  if [ -n "$RT_BOOT" ]; then parts_array+=(/boot="${RT_BOOT%% *}"); fi
+  if [ -n "$RT_HOME" ]; then parts_array+=(/home="${RT_HOME%% *}"); fi
+  if [ -n "$RT_OTHER_PARTS" ]; then parts_array+=($RT_OTHER_PARTS); fi
+  parts_array_sorted=(`for item in "${parts_array[@]}"; do echo "${item//@}"; done | sort -k 1,1 -t =`)
 
-  if [ "$BR_TAB" = "1" ]; then
-    cd /
-    sleep 1
+  umount /mnt/target/dev/pts
+  umount /mnt/target/proc
+  umount /mnt/target/dev
+  umount /mnt/target/sys/firmware/efi/efivars
+  umount /mnt/target/sys
+  umount /mnt/target/run
 
-    if [ -n "$RT_ROOT" ]; then parts_array+=("/"="${RT_ROOT%% *}"); fi
-    if [ -n "$RT_ESP" ]; then parts_array+=("$RT_ESP_MOUNTPOINT"="${RT_ESP%% *}"); fi
-    if [ -n "$RT_BOOT" ]; then parts_array+=(/boot="${RT_BOOT%% *}"); fi
-    if [ -n "$RT_HOME" ]; then parts_array+=(/home="${RT_HOME%% *}"); fi
-    if [ -n "$RT_OTHER_PARTS" ]; then parts_array+=($RT_OTHER_PARTS); fi
-    parts_array_sorted=(`for item in "${parts_array[@]}"; do echo "${item//@}"; done | sort -k 1,1 -t =`)
-
-    umount /mnt/target/dev/pts
-    umount /mnt/target/proc
-    umount /mnt/target/dev
-    umount /mnt/target/sys/firmware/efi/efivars
-    umount /mnt/target/sys
-    umount /mnt/target/run
-
-    if [ -n "$parts_array_sorted" ]; then
-      while read ln; do
-        sleep 1
-        if [ ! -z "$(lsblk -d -n -o mountpoint 2>/dev/null "$ln")" ]; then
-          umount "$ln" || _err="y"
-        fi
-      done < <(for part in "${parts_array_sorted[@]}"; do echo "$part" | cut -f2 -d"="; done | tac)
-    fi
-    if [ -z "$_err" ]; then rm -r /mnt/target; fi
+  if [ -n "$parts_array_sorted" ]; then
+    while read ln; do
+      sleep 1
+      if [ ! -z "$(lsblk -d -n -o mountpoint 2>/dev/null "$ln")" ]; then
+        echo "Unmounting $ln" > /tmp/wr_proc
+        umount "$ln" || _err="y"
+      fi
+    done < <(for part in "${parts_array_sorted[@]}"; do echo "$part" | cut -f2 -d"="; done | tac)
   fi
+  if [ -z "$_err" ]; then rm -r /mnt/target; fi
 }
 
 run_main() {
@@ -324,6 +319,10 @@ run_main() {
     echo false > /tmp/wr_pid
     setsid ./star.sh "${SCR_ARGS[@]}" >&3 2> /tmp/wr_log
     sleep 0.1
+    if [ "$BR_TAB" = "1" ] && grep -qF "Process ID" /tmp/wr_log; then
+      echo Aborting > /tmp/wr_proc
+      clean_umount
+    fi
     if grep -qF [ERROR] /tmp/wr_log; then
       grep -F [ERROR] /tmp/wr_log | cut -f2- -d" " > /tmp/wr_proc
     elif grep -qF [SUMMARY] /tmp/wr_log; then
@@ -839,7 +838,7 @@ lost+found">
                                 <input file stock="gtk-stop"></input>
                                 <variable>BTN_CANCEL</variable>
                                 <label>Cancel</label>
-                                <action>bash -c "source /tmp/wr_functions; kill_umount"</action>
+                                <action>kill -9 -$(tail -1 /tmp/wr_pid) && echo "Process ID $(tail -1 /tmp/wr_pid) killed" > /tmp/wr_log</action>
                         </button>
                         <button tooltip-text="Exit">
                                 <variable>BTN_EXIT</variable>

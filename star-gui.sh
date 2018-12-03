7864fbdbad3a3354f67ce753b2ba97853a40f90d
tritonas00 <tritonas00@gmail.com>
2018-12-03 12:12:10 +0200

    changed text layout

--- star-gui.sh
+++ star-gui.sh
@@ -860,9 +860,9 @@ lost+found">
                                 </edit>
                         </vbox>
                         <vbox>
-                                <text use-markup="true" label="<b><big>System Tar &amp; Restore</big></b>"></text>
-                                <text wrap="false" label="Backup and Restore your system using tar or Transfer it with rsync"></text>
-                                <text use-markup="true" label="<i><small>Version 7.0 tritonas00@gmail.com 2012-2018</small></i>"></text>
+                                <text use-markup="true" label="<big>System Tar &amp; Restore</big>"></text>
+                                <text label="Version 7.0 tritonas00@gmail.com 2012-2018"></text>
+                                <text use-markup="true" label="<small>Backup and Restore your system using tar or Transfer it with rsync</small>"></text>
                                 <hseparator></hseparator>
                                 <vbox scrollable="true" shadow-type="0">
                                         <text xalign="0" wrap="false">
